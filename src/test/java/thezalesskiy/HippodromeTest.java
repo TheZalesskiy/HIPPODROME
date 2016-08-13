50662158de1ab83d643263f7961135d57f459be0
Evgeniy Zalesskiy <evgeniy.zalesskiy@gmail.com>
2016-08-13 18:07:11 +0300

    second commit

---
+++ src/test/java/thezalesskiy/HippodromeTest.java
@@ -0,0 +1,65 @@
+package thezalesskiy;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * HippodromeTest class
+ * 13.08.2016
+ * Created by TheZalesskie
+ */
+public class HippodromeTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+
+    }
+
+    @Test
+    public void getHorses() throws Exception {
+
+    }
+
+    @Test
+    public void addHorse() throws Exception {
+
+    }
+
+    @Test
+    public void run() throws Exception {
+
+    }
+
+    @Test
+    public void move() throws Exception {
+
+    }
+
+    @Test
+    public void print() throws Exception {
+
+    }
+
+    @Test
+    public void getWinner() throws Exception {
+
+    }
+
+    @Test
+    public void printWinner() throws Exception {
+
+    }
+
+}
